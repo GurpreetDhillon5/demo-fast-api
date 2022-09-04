db411531d781eb340aa6c54357f01e557715d7c4
Gurpreet Dhillon <gurpreetdhillon@Gurpreets-MacBook-Air.local>
2022-09-04 12:59:25 -0700

    html support

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import logging
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
@@ -7,7 +11,14 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 app = FastAPI(title = "NBA API")
+templates = Jinja2Templates(directory= "templates")
 
 @app.get("/")
 def read_root():
 	return{"API": "Hello NBA!"}
+
+@app.get("/home", response_class=HTMLResponse)
+def home(request: Request):
+	return templates.TemplateResponse(
+		"index.html", {"request": request}
+	)
