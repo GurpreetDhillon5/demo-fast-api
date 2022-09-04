48a399e995680051de0b39d3878769c2b6022bdc
Gurpreet Dhillon <gurpreetdhillon@Gurpreets-MacBook-Air.local>
2022-09-04 12:31:19 -0700

    demo api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-#Store information about pizza
-pizza = {
-	'crust' : 'thick',
-	'toppings' : ['mushrooms', 'extra cheese'],
-	}
-# Summarize the order
-print("You ordered a " + pizza['crust'] + "-crust pizza " + 
-	"with the following toppings:")
-for topping in pizza['toppings']:
-	print("\t" + topping)
+import logging
+from fastapi import FastAPI
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
+)
+
+log = logging.getLogger(__name__)
+app = FastAPI(title = "NBA API")
+
+@app.get("/")
+def read_root():
+	return{"API": "Hello NBA!"}
